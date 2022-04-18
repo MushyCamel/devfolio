228e4b126ccc575557e4d500c8c0eccbba93f17d
Mark Tempini <marktempini@googlemail.com>
2022-04-18 17:45:44 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -153,7 +153,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`, // This color appears on mobile
         display: `minimal-ui`,
-        icon: `src/images/X.png`,
+        icon: `src/images/X.jpg`,
       },
     },
   ],
