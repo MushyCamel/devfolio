1cb22a627c9e5d83a4654fd1a553cf4ce11deb16
Mark Tempini <marktempini@googlemail.com>
2022-05-10 13:53:52 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -90,7 +90,7 @@ module.exports = {
     ],
     video: [
       {
-        scr: 'src\components\video\Showreal_2021.mp4',
+        scr: '..\components\video\Showreal_2021.mp4',
         title:'Showreel'
       }
     ],
