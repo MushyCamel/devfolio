cf5c6655bb45912d719e9751f912cd0a5ce83dbb
Mark Tempini <marktempini@googlemail.com>
2022-08-24 17:17:36 +0100

    style changes

--- src/templates/blog-post.jsx
+++ src/templates/blog-post.jsx
@@ -9,8 +9,8 @@ import Video from '../components/video/video';
 
 const classes = {
   wrapper: 'mt-16 blog-content',
-  title: 'mt-16 text-4xl text-gray-900 font-bold',
-  date: 'text-gray-600 font-light',
+  title: 'mt-16 text-4xl text-slate-900 font-bold',
+  date: 'text-slate-900 font-light',
 };
 
 const BlogPost = ({ data }) => {
