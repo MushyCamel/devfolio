b2f6e6acd2635766a40e179afac83da202939f91
Mark Tempini <marktempini@googlemail.com>
2022-08-24 17:26:19 +0100

    style tests

--- src/components/summary-item/index.jsx
+++ src/components/summary-item/index.jsx
@@ -2,7 +2,7 @@ import { Link } from 'gatsby';
 import React from 'react';
 
 const classes = {
-  wrapper: 'mb-6',
+  wrapper: 'mb-6 background-color:#7f1d1d',
   name: 'font-semibold text-slate-900 pb-1',
   description: 'text-md text-gray-800 font-light',
 };
@@ -19,7 +19,7 @@ const SummaryItem = ({ name, description, link = false, internal = false }) => {
     <div className={classes.wrapper}>
       <h3
         className={`${classes.name} ${
-          link ? 'hover:underline hover:text-black' : ''
+          link ? 'hover:underline-offset-1 hover:text-black' : ''
         }`}
       >
         {link ? linkContent : name}
