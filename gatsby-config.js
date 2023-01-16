e812e681e3415fd4afa1e58cc915abe8d3837f9d
Mark Tempini <marktempini@googlemail.com>
2023-01-16 15:37:47 +0000

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -81,11 +81,11 @@ module.exports = {
       {
         name: 'Production',
         description:
-          '- Strong experience leading and coordinating teams from initial development through launch.' + "<br>" +
-          '- Exceptional verbal, written, and interpersonal communication skills.' + "<br>" +
-          '- Expertise in common Production methodologies (eg. Agile, Waterfall) as well as software (Monday, ShotGrid, Excel, Microsoft Project, Confluence, and in-house software)' + "<br>" +
-          '- Proven experience in a production role where I have supported cross-functional teams in the creation and delivery of sophisticated projects '+ "<br>" +
-          '- Excellent organizational & prioritization skills, attention to detail, process knowledge and ability to multi-task',
+          '- Strong experience leading and coordinating teams from initial development through launch.\
+          - Exceptional verbal, written, and interpersonal communication skills.\
+          - Expertise in common Production methodologies (eg. Agile, Waterfall) as well as software (Monday, ShotGrid, Excel, Microsoft Project, Confluence, and in-house software)\
+          - Proven experience in a production role where I have supported cross-functional teams in the creation and delivery of sophisticated projects \
+          - Excellent organizational & prioritization skills, attention to detail, process knowledge and ability to multi-task'
       },
       {
         name: 'Design',
